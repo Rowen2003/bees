6ed4b71c74804721f149fa36c8b7c418206d8246
Dolankski <dylankaspick@gmail.com>
2024-01-19 17:32:26 -0600

    Player Controller
    Player movement is in a good enough state for now. Can be improved at a later date, time permitting.

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -4,9 +4,9 @@ extends CharacterBody3D
 @export var speed = 5.0
 
 var input_dir : Vector3
-var rotating = false
-var prev_mouse_position
-var next_mouse_position 
+
+func _ready():
+	pass
 
 func _unhandled_input(event: InputEvent):
 	if (Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)):
@@ -14,7 +14,8 @@ func _unhandled_input(event: InputEvent):
 		
 		if event is InputEventMouseMotion:
 			rotate_y(-event.relative.x * 0.01)
-			rotate_x(event.relative.y * 0.01)
+			rotate_object_local(Vector3(1,0,0), -event.relative.y * 0.01)
+
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
@@ -23,17 +24,17 @@ func _physics_process(delta):
 	input_dir = Vector3.ZERO
 	
 	if (Input.is_key_pressed(Key.KEY_A)):
-		input_dir += Vector3.MODEL_LEFT
+		input_dir += Vector3.LEFT
 	if (Input.is_key_pressed(Key.KEY_D)):
-		input_dir += Vector3.MODEL_RIGHT
+		input_dir += Vector3.RIGHT
 	if (Input.is_key_pressed(Key.KEY_SPACE)):
-		input_dir += Vector3.MODEL_TOP
+		input_dir += Vector3.UP
 	if (Input.is_key_pressed(Key.KEY_CTRL)):
-		input_dir += Vector3.MODEL_BOTTOM
+		input_dir += Vector3.DOWN
 	if (Input.is_key_pressed(Key.KEY_W)):
-		input_dir += Vector3.MODEL_FRONT
+		input_dir += Vector3.FORWARD
 	if (Input.is_key_pressed(Key.KEY_S)):
-		input_dir += Vector3.MODEL_REAR
+		input_dir += Vector3.BACK
 	
 	
 	var direction = (transform.basis * Vector3(input_dir.x, input_dir.y, input_dir.z)).normalized()
@@ -45,5 +46,5 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.y = move_toward(velocity.y, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-
+		
 	move_and_slide()
